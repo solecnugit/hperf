1824a9c961a99d8415abe89d1c908db1af43097a
huanlun <m13955972978_2@163.com>
2022-11-29 09:36:33 +0000

    add some virtual funcs in Connector/Connector.py

--- Connector/Connector.py
+++ Connector/Connector.py
@@ -1,12 +1,42 @@
 class Connector:
     """
-    the interface of Connector
+    the Abstract class of all Connectors.
     """
+
     def __init__(self, configs: dict) -> None:
         self.configs = configs
 
-    def run_command(self, command: str) -> str:
+    """
+        Run profile bash script file.
+        You should call this func only when you want to run a profiling command.
+    """
+
+    def run_command_with_file(self, command: str) -> str:
         pass
 
-    def get_tmp_file_path(self, tmp_file_path: str) -> str:
+    """
+        Call this func after call run_command_with_file to get perf result.
+    """
+
+    def get_result(self) -> str:
+        pass
+
+    """
+        Call this func after call run_command_with_file to get perf err.
+    """
+
+    def get_err(self) -> str:
+        pass
+
+    """
+        Check wheather pmc is occupied.
+    """
+
+    def runtime_check(self) -> bool:
+        pass
+
+    def get_cpu_architecture(self) -> str:
+        pass
+
+    def clear(self):
         pass
