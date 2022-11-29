8b7a3e1428d35bf015be4c2a7ee22f504420dc7c
huanlun <m13955972978_2@163.com>
2022-11-29 12:45:09 +0000

    replace os.system() with subprocess.Popen()

--- Connector/LocalConnector.py
+++ Connector/LocalConnector.py
@@ -1,5 +1,6 @@
 from Connector.Connector import Connector
 from datetime import datetime
+import subprocess
 import os
 
 
@@ -10,16 +11,23 @@ class LocalConnector(Connector):
         """
         super(LocalConnector, self).__init__(configs)
         self.tmp_dir = self.configs["tmp_dir"]
-        self.file_name = "hperf_{}.sh".format(datetime.now)
-        self.path = self.tmp_dir + "/hperf.sh"
+        self.file_name = "hperf_{}.sh".format(datetime.now().strftime('%Y-%m-%d'))
+        self.path = self.tmp_dir + self.file_name
 
     def run_command_with_file(self, command: str):
         self.__pre_bash_file__(command)
-        os.system("cd {} && bash bash.sh".format(self.tmp_dir))
+        perf_process =  subprocess.Popen(["bash", "{}".format(self.path)], stdout=subprocess.PIPE)
+        perf_process.wait()
 
     def get_result(self) -> str:
-        result_lines = ""
-        result_path = ""
+        ls_res = subprocess.Popen("cd {} && ls".format(self.tmp_dir), stdout=subprocess.PIPE, shell=True)
+        grep_res = subprocess.Popen("grep hperf_perf_re", stdin=ls_res.stdout, stdout=subprocess.PIPE, shell=True)
+        ls_res.stdout.close()
+        res = grep_res.stdout
+        result_path = res.read()
+        result_path = result_path.decode("utf-8")
+        result_path = result_path.strip()
+        result_path = self.tmp_dir + "/" + result_path
         with open(result_path, 'r') as f:
             result_lines = f.readlines()
         result = ""
@@ -28,10 +36,10 @@ class LocalConnector(Connector):
         return result
 
     def get_err(self) -> str:
-        raise NotImplemented("Undone!")
+        return " "
 
     def clear(self):
-        pass
+        subprocess.Popen("cd {} && rm hperf* && kill -2 $(pgrep perf)".format(self.tmp_dir), shell=True)
 
     """
         Undone! Just return Icelacke.
