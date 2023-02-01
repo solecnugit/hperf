b31d26634729f1ab0beafd60afe028f403db7c9d
GraysonLiu <graysonliu@foxmail.com>
2023-02-01 17:31:38 +0000

    docs: (SOLE/hperf#21) collate and add comments

--- controller.py
+++ controller.py
@@ -11,35 +11,37 @@ from event_group import EventGroup
 
 class Controller:
     """
-    'Controller' is responsible for process control of hperf.
-    Users can conduct profiling by calling 'hperf()' method after instantiate 'Controller'.
+    `Controller` is responsible for the whole process control of hperf.
+    Users can conduct profiling for workload by calling `hperf()` method after instantiate `Controller`.
     """
-    VERSION = "v1.0.1"
+    VERSION = "v1.1.0"
 
     def __init__(self, argv: Sequence[str]):
         """
-        Constructor of 'Controller'.
-        :param argv: options and parameters passed from command line when invoke 'hperf'. 
-        Usually, it should be 'sys.argv[1:]' (since 'sys.argv[0]' is 'hperf.py').
+        Constructor of `Controller`.
+        :param `argv`: options and parameters passed from command line when invoke `hperf`. 
+        Usually, it should be `sys.argv[1:]` (since `sys.argv[0]` is `hperf.py`).
         """
         self.argv = argv    # the original command line options and parameters
 
         self.configs = {}    # a dict contains parsed configurations for the following steps
         self.parser = OptParser()
 
-        self.connector: Connector = None    # an instance of 'Connector'
-        self.profiler: Profiler = None    # an instance of 'Profiler'
-        self.analyzer: Analyzer = None    # an instance of 'Analyzer'
-        self.event_groups: EventGroup = None    # an instancce of 'EventGroup'
+        self.connector: Connector = None
+        self.profiler: Profiler = None
+        self.analyzer: Analyzer = None
+        self.event_groups: EventGroup = None
 
-        # initialize Logger
-        # Note: Since Logger follows singleton pattern, it is unnecessary to pass the reference of the instance of Logger to other modules / classes.
-        # When we need to log records, use the method logging.getLogger(<name>) provided by module logging to get the instance of Logger,
-        # where it will get the very same instance of Logger as long as the <name> is same.
+        # initialize `Logger`
+        # Note: Since `Logger` follows singleton pattern, 
+        # it is unnecessary to pass the reference of the instance of `Logger` to other classes throught their constructor.
+        # When we need to log records, use the method `logging.getLogger(<name>)` provided by module logging to get the instance of `Logger`,
+        # where it will get the very same instance of `Logger` as long as the `<name`> is same.
         self.logger = logging.getLogger("hperf")
         self.logger.setLevel(logging.DEBUG)
         formatter = logging.Formatter("%(asctime)-15s %(levelname)-8s %(message)s")
 
+        # logs will output to both console and file
         # [1] for console output of logs
         self.__handler_stream = logging.StreamHandler()
         self.__handler_stream.setFormatter(formatter)
@@ -56,42 +58,54 @@ class Controller:
     def hperf(self):
         """
         This method covers the whole process of profiling.
+        Call this method to start profiling for workloads.
         """
         self.logger.info(f"hperf {Controller.VERSION}")
 
-        # Step 1. parse the original command line options and parameters
+        # step 1.
         self.__parse()
 
-        # Step 2. conduct profiling by executing script with 'perf stat ...' command
-        # raw performance data will be saved in the temporary directory
+        # step 2.
         self.__profile()
 
-        # Step 3. analyze the raw performance data and output realiable performance metrics
+        # step 3.
         self.__analyze()
 
+        # save logs to file in the local test directory
+        # TODO: in future, `Controller` is responsible for unified exceptional handling, so that this method will be useful in `finally` blocks
         self.__save_log_file()
     
     def __parse(self):
         """
-        Parse the original command line options and parameters and get the configuration dict.
-        Based on the configuration dict, initialze a 'Connector' for 'Profiler' and 'Analyzer'.
+        Parse the original command line options and arguments (`.argv`) and then get the configuration dict (`.configs`). 
+        Based on the configuration dict, validate the configurations. 
+        If passed the validation, instantiate `Connector` (`.connector`). 
         """
+        # step 1.1. parse the original command line options and arguments
         self.configs = self.parser.parse_args(self.argv)
 
-        # if verbosity is declared, change the threshold of log level to print to console
+        # step 1.2. if verbosity is declared (`-v` option), change the threshold of log level to print to console:
+        # log level: DEBUG < INFO < WARNING < ERROR < CRITICAL
+        # for file: always > DEBUG, not affected by `-v` option
+        # for console: > INFO (default), > DEBUG (if verbosity is declared)
         if "verbose" in self.configs:
             self.__handler_stream.setLevel(logging.DEBUG)
         
+        # step 1.3. validate the configurations
+        # TODO: in future, more the validation of parsed configurations should be added here
         # if command is empty, exit the program
         if "command" not in self.configs:
             self.logger.error("workload is not specified")
             exit(-1)
         
+        # step 1.4. instantiate `Connector`
         self.connector = self.__get_connector()
 
     def __get_connector(self) -> Connector:
         """
-        Depend on the parsed configurations, initialize a 'LocalConnector' or 'RemoteConnector'
+        Depend on the parsed configurations (`.configs`), instantiate a `LocalConnector` or `RemoteConnector` (`.connector`). 
+        `LocalConnector` provides useful methods to interact with the local host, while `RemoteConnector` is for remote host by SSH. 
+        `LocalConnector` and `RemoteConnector` are extended from interface `Connector`, which defined useful methods for other modules to use. 
         """
         # Note: the instantiation of 'Connector' may change the value of 'self.configs["tmp_dir"]' 
         # if the parsed temporary directory is invalid (cannot be accessed).
@@ -104,10 +118,17 @@ class Controller:
 
     def __profile(self):
         """
-        Firstly run sanity check, then enerate and execute profiling script, where the raw performance data is saved in the temporary directory.
+        Instantiate `EventGroup` (`.event_groups`) and `Profiler` (`.profiler`) based on the parsed configurations (`.configs`). 
+        Then call the methods of `Profiler`:
+        1) run sanity check, 
+        2) generate and execute profiling script on SUT and save the raw performance data in the test directory 
+        (a sub-directory in the temporary directory which can be obtained by `.connector.get_test_dir_path()` method), 
+        3) if the `.connector` is an instance of `RemoteConnector`, close SSH / SFTP connection between remote SUT and local host
         """
         self.event_groups = EventGroup(self.connector)
         self.profiler = Profiler(self.connector, self.configs, self.event_groups)
+        
+        # step 2.1. sanity check
         if not self.profiler.sanity_check():
             select = input("Detected some problems which may interfere profiling. Continue profiling? [y|N] ")
             while True:
@@ -120,9 +141,11 @@ class Controller:
                     select = input("please select: [y|N] ")
         else:
             self.logger.info("sanity check passed.")
+        
+        # step 2.2. profile
         self.profiler.profile()
         
-        # for RemoteConnector, close SSH / SFTP connection between remote SUT and local host
+        # step 2.3. for RemoteConnector, close SSH / SFTP connection between remote SUT and local host
         if isinstance(self.connector, RemoteConnector):
             self.connector.sftp.close()
             self.connector.client.close()
