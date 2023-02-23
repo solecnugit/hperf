32643eefcc066574f4e4c19c6e7dbe98d4f98f57
GraysonLiu <graysonliu@foxmail.com>
2023-02-23 14:22:34 +0800

    feat(arch): add support of TLB-related metrics for ARM and Intel

--- arch/arm.py
+++ arch/arm.py
@@ -1,97 +1,69 @@
 events = [
-    {
-        "id": 0,
-        "perf_name": "cpu-clock",
-        "name": "CPU TIME"
-    },
-    {
-        "id": 1,
-        "perf_name": "duration_time",
-        "name": "WALL CLOCK TIME",
-        "type": "SYSTEM"
-    },
-    {
-        "id": 20,
-        "perf_name": "cycles",
-        "name": "CYCLES"
-    },
-    {
-        "id": 30,
-        "perf_name": "instructions",
-        "name": "INSTRUCTIONS"
-    },
-    {
-        "id": 31,
-        "perf_name": "r01",
-        "name": "L1I CACHE MISSES"
-    },
-    {
-        "id": 32,
-        "perf_name": "r03",
-        "name": "L1D CACHE MISSES"
-    },
-    {
-        "id": 33,
-        "perf_name": "r17",
-        "name": "L2 CACHE MISSES"
-    },
-    {
-        "id": 34,
-        "perf_name": "r2a",
-        "name": "L3 CACHE MISSES"
-    },
-    {
-        "id": 35,
-        "perf_name": "r21",
-        "name": "BRANCHES"
-    },
-    {
-        "id": 36,
-        "perf_name": "r22",
-        "name": "BRANCH MISSES"
-    }
+    # OS
+    { "id": 0, "perf_name": "cpu-clock", "name": "CPU TIME" },
+    { "id": 1, "perf_name": "duration_time", "name": "WALL CLOCK TIME", "type": "SYSTEM" },
+    { "id": 2, "perf_name": "cs", "name": "CONTEXT SWITCH" },
+    # PMU - General
+    { "id": 20, "perf_name": "cycles", "name": "CYCLES" },
+    { "id": 21, "perf_name": "instructions", "name": "INSTRUCTIONS" },
+    # PMU - Cache
+    { "id": 30, "perf_name": "r01", "name": "L1I CACHE MISSES" },
+    { "id": 31, "perf_name": "r14", "name": "L1I CACHE ACCESSES" },
+    { "id": 32, "perf_name": "r03", "name": "L1D CACHE MISSES" },
+    { "id": 33, "perf_name": "r04", "name": "L1D CACHE ACCESSES" },
+    { "id": 34, "perf_name": "r17", "name": "L2 CACHE MISSES" },
+    { "id": 35, "perf_name": "r16", "name": "L2 CACHE ACCESSES" },
+    # PMU - LLC (uncore)
+    # { "id": 100, "perf_name": "arm_cmn_0/hnf_cache_miss/", "name": "LL CACHE MISSES", "type": "SOCKET" },
+    # { "id": 101, "perf_name": "arm_cmn_0/hnf_slc_sf_cache_access/", "name": "LL CACHE ACCESSES", "type": "SOCKET" },
+    # PMU - Branch
+    { "id": 40, "perf_name": "r22", "name": "BRANCH MISSES" }, 
+    { "id": 41, "perf_name": "r21", "name": "BRANCHES" },
+    # PMU - TLB
+    { "id": 50, "perf_name": "r35", "name": "ITLB WALKS" }, 
+    { "id": 51, "perf_name": "r26", "name": "ITLB ACCESSES" },
+    { "id": 52, "perf_name": "r34", "name": "DTLB WALKS" },
+    { "id": 53, "perf_name": "r25", "name": "DTLB ACCESSES" },
+    # PMU - Other
+    { "id": 60, "perf_name": "r23", "name": "FRONTEND STALLS" }, 
+    { "id": 61, "perf_name": "r24", "name": "BACKEND STALLS" }
 ]
 
-other_events = [0, 1]
+other_events = [0, 1, 2]
 
-pinned_events = [20]
+pinned_events = [20, 21]
 
+# left 5 counters for other events
 event_groups = [
-    [30, 31, 32, 33, 34],
-    [35, 36]
+    [30, 31, 32, 33],
+    # [34, 35, 40, 41, 60, 100, 101],
+    [34, 35, 40, 41, 60],
+    [50, 51, 52, 53, 61]
 ]
 
 metrics = [
-    {
-        "metric": "CPU UTILIZATION",
-        "expression": "e0 / (e1 / 1000000)"
-    },
-    {
-        "metric": "CPI",
-        "expression": "e20 / e30"
-    },
-    {
-        "metric": "FREQUENCY",
-        "expression": "e20 / (e1 / 1000000000)"
-    },
-    {
-        "metric": "L1I CACHE MPKI",
-        "expression": "(1000 * e31) / e30"
-    },
-    {
-        "metric": "L1D CACHE MPKI",
-        "expression": "(1000 * e32) / e30"
-    },
-    {
-        "metric": "L2 CACHE MPKI",
-        "expression": "(1000 * e33) / e30"
-    },
-    {
-        "metric": "L3 CACHE MPKI",
-        "expression": "(1000 * e34) / e30"
-    },
-    {
-        "metric": "BRANCH MISS RATE",
-        "expression": "e36 / e35"
-    }
+    # General
+    { "metric": "CPU UTILIZATION", "expression": "e0 / (e1 / 1000000)" },
+    { "metric": "CPI", "expression": "e20 / e21" },
+    { "metric": "FREQUENCY", "expression": "e20 / (e1 / 1000000000)" },
+    # Cache
+    { "metric": "L1I CACHE MPKI", "expression": "(1000 * e30) / e21" },
+    { "metric": "L1I CACHE MISS RATE", "expression": "e30 / e31" },
+    { "metric": "L1D CACHE MPKI", "expression": "(1000 * e32) / e21" },
+    { "metric": "L1D CACHE MISS RATE", "expression": "e32 / e33" },
+    { "metric": "L2 CACHE MPKI", "expression": "(1000 * e34) / e21" },
+    { "metric": "L2 CACHE MISS RATE", "expression": "e34 / e35" },
+    # { "metric": "L3 CACHE MPKI", "expression": "(1000 * e100) / e21" },
+    # { "metric": "L3 CACHE MISS RATE", "expression": "e100 / e101" },
+    # Branch
+    { "metric": "BRANCH MPKI", "expression": "(1000 * e40) / e21" },
+    { "metric": "BRANCH MISS RATE", "expression": "e40 / e41" },
+    # TLB
+    { "metric": "ITLB MPKI", "expression": "(1000 * e50) / e21" },
+    { "metric": "DTLB MPKI", "expression": "(1000 * e52) / e21" },
+    { "metric": "ITLB WALK RATE", "expression": "e50 / e51" },
+    { "metric": "DTLB WALK RATE", "expression": "e52 / e53" },
+    # Stall
+    { "metric": "FRONTEND STALL RATE", "expression": "e60 / e20" },
+    { "metric": "BACKEND STALL RATE", "expression": "e61 / e20" }
 ]
--- arch/intel_icelake.py
+++ arch/intel_icelake.py
@@ -1,98 +1,61 @@
 events = [
-    {
-        "id": 0,
-        "perf_name": "cpu-clock",
-        "name": "CPU TIME"
-    },
-    {
-        "id": 1,
-        "perf_name": "duration_time",
-        "name": "WALL CLOCK TIME",
-        "type": "SYSTEM"
-    },
-    {
-        "id": 10,
-        "perf_name": "msr/tsc/",
-        "name": "TSC",
-    },
-    {
-        "id": 20,
-        "perf_name": "cycles",
-        "name": "CYCLES"
-    },
-    {
-        "id": 21,
-        "perf_name": "instructions",
-        "name": "INSTRUCTIONS"
-    },
-    {
-        "id": 22,
-        "perf_name": "ref-cycles",
-        "name": "REFERENCE CYCLES"
-    },
-    {
-        "id": 30,
-        "perf_name": "r08d1",
-        "name": "L1 CACHE MISSES"
-    },
-    {
-        "id": 31,
-        "perf_name": "r10d1",
-        "name": "L2 CACHE MISSES"
-    },
-    {
-        "id": 32,
-        "perf_name": "r20d1",
-        "name": "L3 CACHE MISSES"
-    },
-    {
-        "id": 33,
-        "perf_name": "r00c4",
-        "name": "BRANCHES"
-    },
-    {
-        "id": 34,
-        "perf_name": "r00c5",
-        "name": "BRANCH MISSES"
-    }
+    # OS
+    { "id": 0, "perf_name": "cpu-clock", "name": "CPU TIME" },
+    { "id": 1, "perf_name": "duration_time", "name": "WALL CLOCK TIME", "type": "SYSTEM" },
+    { "id": 2, "perf_name": "cs", "name": "CONTEXT SWITCHES" },
+    # MSR
+    { "id": 10, "perf_name": "msr/tsc/", "name": "TSC" },
+    # PMU - General
+    { "id": 20, "perf_name": "cycles", "name": "CYCLES" },
+    { "id": 21, "perf_name": "instructions", "name": "INSTRUCTIONS" },
+    { "id": 22, "perf_name": "ref-cycles", "name": "REFERENCE CYCLES" },
+    # PMU - Cache
+    { "id": 30, "perf_name": "r08d1", "name": "L1 CACHE MISSES" },    # MEM_LOAD_RETIRED.L1_MISS
+    { "id": 31, "perf_name": "r01d1", "name": "L1 CACHE HITS" },    # MEM_LOAD_RETIRED.L1_HIT
+    { "id": 32, "perf_name": "r10d1", "name": "L2 CACHE MISSES" },    # MEM_LOAD_RETIRED.L2_MISS
+    { "id": 33, "perf_name": "r02d1", "name": "L2 CACHE HITS" },    # MEM_LOAD_RETIRED.L2_HIT
+    # PMU - LLC (uncore)
+    { "id": 100, "perf_name": "uncore_cha_0/event=0x34,umask=0x1fe001/", "name": "LL CACHE MISSES", "type": "SOCKET" },    # LLC_LOOKUP.MISS_ALL
+    { "id": 101, "perf_name": "uncore_cha_0/event=0x34,umask=0x1fffff/", "name": "LL CACHE ACCESSES", "type": "SOCKET" },    # LLC_LOOKUP
+    # PMU - Branch 
+    { "id": 40, "perf_name": "r00c5", "name": "BRANCH MISSES" },    # BR_MISP_RETIRED.ALL_BRANCHES
+    { "id": 41, "perf_name": "r00c4", "name": "BRANCHES" },    # BR_INST_RETIRED.ALL_BRANCHES
+    # PMU - TLB
+    { "id": 50, "perf_name": "r0e85", "name": "ITLB WALKS" },     # ITLB_MISSES.WALK_COMPLETED
+    #     ITLB ACCESSES = INSTRUCTIONS
+    { "id": 51, "perf_name": "r0e08", "name": "DTLB LOAD WALKS" },     # DTLB_LOAD_MISSES.WALK_COMPLETED
+    { "id": 52, "perf_name": "r0e49", "name": "DTLB STORE WALKS" },     # DTLB_STORE_MISSES.WALK_COMPLETED
+    #     DTLB WALKS = DTLB LOAD WALKS + DTLB STORE WALKS
+    { "id": 53, "perf_name": "r83d0", "name": "DTLB ACCESSES" }    # MEM_INST_RETIRED.ANY
 ]
 
-other_events = [0, 1, 10]
+other_events = [0, 1, 2, 10]
 
 pinned_events = [20, 21, 22]
 
 event_groups = [
-    [30, 31, 32],
-    [33, 34]
+    [30, 31, 32, 33, 100, 101],
+    [50, 51, 52, 53, 40, 41]
 ]
 
 metrics = [
-    {
-        "metric": "CPU UTILIZATION",
-        "expression": "e22 / e10"
-    },
-    {
-        "metric": "FREQUENCY",
-        "expression": "e20 / (e1 / 1000000000)"
-    },
-    {
-        "metric": "CPI",
-        "expression": "e20 / e21"
-    },
-    {
-        "metric": "L1 CACHE MPKI",
-        "expression": "(1000 * e30) / e21"
-    },
-    {
-        "metric": "L2 CACHE MPKI",
-        "expression": "(1000 * e31) / e21"
-    },
-    {
-        "metric": "L3 CACHE MPKI",
-        "expression": "(1000 * e32) / e21"
-    },
-    {
-        "metric": "BRANCH MISS RATE",
-        "expression": "e34 / e33"
-    }
+    # General
+    { "metric": "CPU UTILIZATION", "expression": "e22 / e10" },
+    { "metric": "FREQUENCY", "expression": "e20 / (e1 / 1000000000)" },
+    { "metric": "CPI", "expression": "e20 / e21" },
+    # Cache
+    { "metric": "L1 CACHE MPKI", "expression": "(1000 * e30) / e21" },
+    { "metric": "L1 CACHE MISS RATE", "expression": "e30 / (e30 + e31)" },
+    { "metric": "L2 CACHE MPKI", "expression": "(1000 * e32) / e21" },
+    { "metric": "L2 CACHE MISS RATE", "expression": "e32 / (e32 + e33)" },
+    # LLC
+    { "metric": "LL CACHE MPKI", "expression": "(1000 * e100) / e21" },
+    { "metric": "LL CACHE MISS RATE", "expression": "e100 / e101" },
+    # Branch
+    { "metric": "BRANCH MPKI", "expression": "(1000 * e40) / e21" },
+    { "metric": "BRANCH MISS RATE", "expression": "e40 / e41" },
+    # TLB
+    { "metric": "ITLB MPKI", "expression": "(1000 * e50) / e21" },
+    { "metric": "DTLB MPKI", "expression": "(1000 * (e51 + e52)) / e21" },
+    { "metric": "DTLB WALK RATE", "expression": "(e51 + e52) / e53" },
 ]
