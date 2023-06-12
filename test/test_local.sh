5de35290be04798daaa0a689b75785f4efcce1b5
GraysonLiu <graysonliu@foxmail.com>
2023-06-12 15:28:44 +0800

    refactor(test): remove unused scripts

--- test/test_local.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-TMP_DIR=/home/tongyu/tmp
-workload_output=$(mktemp -p $TMP_DIR -t hperf_workload_output.XXXXXX)
-nohup taskset -c 6 ./mat_mul 128 > "$workload_output" 2>&1 &
-workload_pid=$!
-
-python ../hperf.py -l -p $workload_pid -c 6 --tmp-dir $TMP_DIR --metrics CPI,MPI 
