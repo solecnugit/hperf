ea43478a4f89b1d1ede469c38bd34ad328165723
GraysonLiu <graysonliu@foxmail.com>
2023-06-12 13:56:58 +0800

    fix(event_group): correct the constructor of EventGroup
    Python does not support overloading __init__().
    Using @classmethod instead.

---
+++ test/analyzer_test.py
@@ -0,0 +1,36 @@
+import sys, importlib, os
+
+if __name__ == "__main__":
+    # modify `tmp_dir` and `test_id` to 
+    tmp_dir = "/home/tongyu/project/hperf/test"
+    test_id = "20230612_test001"
+    
+    test_dir_path = os.path.join(tmp_dir, test_id)
+
+    configs = {
+        "verbose": True,
+        "command": "sleep 10",
+        "host_type": "local", 
+        "cpu_list": "all", 
+        "tmp_dir": tmp_dir,
+        "time": 10
+    }
+
+    sys.path.append("/home/tongyu/project/hperf")
+    event_group_module = importlib.import_module("event_group")
+    analyzer_mmodule = importlib.import_module("analyzer")
+    
+    EventGroup = getattr(event_group_module, "EventGroup")
+
+    event_groups = EventGroup.get_event_group(isa="x86_64", arch="intel_icelake")
+
+    print(event_groups.get_event_groups_str())
+
+    Analyzer = getattr(analyzer_mmodule, "Analyzer")
+
+    analyzer = Analyzer(test_dir_path, configs, event_groups)
+
+    analyzer.analyze()
+    print(analyzer.get_timeseries(to_csv=True))
+    print(analyzer.get_aggregated_metrics(to_csv=True))
+    analyzer.get_timeseries_plot()
